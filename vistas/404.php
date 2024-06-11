67385c96286026446f8e53af65e601f727ca4d05
Benjamin Peña <144568088+BenjaminCoderSW@users.noreply.github.com>
2024-06-11 12:59:01 -0600

    Vista 404 mejorada

--- vistas/404.php
+++ vistas/404.php
@@ -1,1 +1,37 @@
-404
+<!-- Estilos para la vista 404 -->
+<style>
+    .error-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        text-align: center;
+        background-color: #f8f9fa;
+        padding: 20px;
+    }
+    .error-logo {
+        width: 100px;
+    }
+    .error-title {
+        font-size: 10rem;
+        font-weight: bold;
+        color: #343a40;
+    }
+    .error-message {
+        font-size: 1.5rem;
+        color: #6c757d;
+    }
+    .error-button {
+        margin-top: 30px;
+    }
+</style>
+    
+<div class="error-container">
+    <div class="error-content">
+        <div class="error-title">404</div>
+        <img src="./img/logo.png" alt="Logotipo de la empresa" class="error-logo">
+        <div class="error-message">Lo sentimos, la página que buscas no se pudo encontrar.</div>
+        <a href="index.php" class="btn btn-danger error-button">Volver al Inicio</a>
+    </div>
+</div>

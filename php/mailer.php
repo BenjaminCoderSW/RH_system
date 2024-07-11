67d5f1430fdefd832f1d832264bd73e6efe6879a
Benjamin Peña <144568088+BenjaminCoderSW@users.noreply.github.com>
2024-07-11 13:22:40 -0600

    Se añadio mi correo en mailer.php para mi

--- php/mailer.php
+++ php/mailer.php
@@ -33,4 +33,32 @@ function enviar_correo($asunto, $cuerpo, $destinatario) {
         return false;
     }
 }
+
+// Esto es para enviar el correo con mi correo solo sustituir el try catch
+// try {
+//     // Configuración del servidor
+//     $mail->isSMTP();
+//     $mail->Host       = 'smtp.hostinger.com'; // Cambia esto al servidor SMTP que estés usando
+//     $mail->SMTPAuth   = true;
+//     $mail->Username   = 'inventariosmart@cinetickett.com'; // Cambia esto por tu correo
+//     $mail->Password   = 'Hola123.'; // Cambia esto por tu contraseña
+//     $mail->SMTPSecure = 'ssl';
+//     $mail->Port       = 465;
+
+//     // Configuración del remitente y destinatario
+//     $mail->setFrom('inventariosmart@cinetickett.com', 'Sistema RH Atzco');
+//     $mail->addAddress($destinatario);
+
+//     // Contenido del correo
+//     $mail->isHTML(true);
+//     $mail->Subject = $asunto;
+//     $mail->Body    = $cuerpo;
+
+//     $mail->send();
+//     return true;
+// } catch (Exception $e) {
+//     echo 'No se pudo enviar el correo de verificación. Error: ', $mail->ErrorInfo;
+//     return false;
+// }
 ?>
+
